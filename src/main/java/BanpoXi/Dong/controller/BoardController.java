17d6d9e6e98feacd20fa1c9e4b7c99bbc0920109
dong <ultraraptor@naver.com>
2021-04-07 17:35:30 +0900

    리스트 출력

--- src/main/java/BanpoXi/Dong/controller/BoardController.java
+++ src/main/java/BanpoXi/Dong/controller/BoardController.java
@@ -5,16 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/board")
 public class BoardController {
-    //
+
     @GetMapping("/lists")
-    @ResponseBody
     public List<Card> allLists() {
         List<Card> Lists = new ArrayList<>();
         Lists.add(new Card(1, "민초아몬드", "마시쪙민초", "TODO"));
@@ -24,34 +24,35 @@ public class BoardController {
         Lists.add(new Card(5, "윈도우 시스템해", "킹버그헌", "DONE"));
         Lists.add(new Card(6, "러닝리엑트공", "알렉스벵크스한빛", "DONE"));
         Lists.add(new Card(7, "윤성우열혈", "윈도우즈시스", "DONE"));
+        Lists.add(new Card(8, "해리포터", "조앤k롤링", "TODO"));
+        Lists.add(new Card(9, "나는 애플로출근한다", "그래", "PROGRESS"));
         return Lists;
     }
 
     @GetMapping("/todo")
-    @ResponseBody
     public List<Card> todoLists() {
         List<Card> todoLists = new ArrayList<>();
         todoLists.add(new Card(1, "민초아몬드", "마시쪙민초", "TODO"));
         todoLists.add(new Card(2, "아반떼와횬대차", "테슬라사라", "TODO"));
+        todoLists.add(new Card(8, "해리포터", "조앤k롤링", "TODO"));
         return todoLists;
     }
 
+    @GetMapping("/progress")
+    public List<Card> progressLists() {
+        List<Card> progressLists = new ArrayList<>();
+        progressLists.add(new Card(3, "알래스카무한도전", "무야호할아버지", "PROGRESS"));
+        progressLists.add(new Card(4, "우리참별나고", "이상한사이야", "PROGRESS"));
+        progressLists.add(new Card(9, "나는 애플로출근한다", "그래", "PROGRESS"));
+        return progressLists;
+    }
+
     @GetMapping("/done")
-    @ResponseBody
-    public List<Card> todoLists() {
-
+    public List<Card> doneLists() {
+        List<Card> doneLists = new ArrayList<>();
+        doneLists.add(new Card(5, "윈도우 시스템해", "킹버그헌", "DONE"));
+        doneLists.add(new Card(6, "러닝리엑트공", "알렉스벵크스한빛", "DONE"));
+        doneLists.add(new Card(7, "윤성우열혈", "윈도우즈시스", "DONE"));
+        return doneLists;
     }
-
-    @GetMapping("/todo")
-    @ResponseBody
-    public List<Card> todoLists() {
-
-    }
-
-    @GetMapping("/todo")
-    @ResponseBody
-    public List<Card> todoLists() {
-
-    }
-
 }
